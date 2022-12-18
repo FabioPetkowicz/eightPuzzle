84829d02774089ece3945ef4c7475e266fb03b81
Fábio Petkowicz <fopetkowicz@gmail.com>
2022-12-18 08:57:50 -0300

    little_advance_n01: nodo recebendo lista

--- modulo_estruturas/tree.py
+++ modulo_estruturas/tree.py
@@ -19,19 +19,32 @@ class Tree:
             self.root = None
 
 
-
-
 if __name__ == "__main__":
+    '''
     tree = Tree(1)
     tree.root.sun[0] = 0
     tree.root.sun[1] = 1
     tree.root.sun[2] = 2
     tree.root.sun[3] = 3
-    ##n1 = Node(1)
-    ##tree.root = n1
 
     print(tree.root)
     print(tree.root.sun[0])
     print(tree.root.sun[1])
     print(tree.root.sun[2])
     print(tree.root.sun[3])
+    '''
+    tree2 = Tree([[1, 2, 3],[4, 5, 6],[7, 8, 0]])
+    #tree2.root = -1 #para caso de arvore com inicialização nulla;
+    #talves seja descartada essa hipótese, pois agora o importante é funcionar
+
+    tree2.root.sun[0] = []
+    tree2.root.sun[1] = 1
+    tree2.root.sun[2] = 2
+    tree2.root.sun[3] = 3
+
+    print(tree2.root)
+    print(tree2.root.sun[0])
+    print(tree2.root.sun[1])
+    print(tree2.root.sun[2])
+    print(tree2.root.sun[3])
+
